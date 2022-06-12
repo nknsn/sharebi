5281ad4cb42f444efbf58069ed054c780fba1164
Narumi Nakanishi <nknsn21@gmail.com>
2022-06-12 15:03:20 +0000

    [Add]view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to root_path
+    user=User.find_by(email: session_params[:email])
+    if user&.authenticate(session_params[:password])#ユーザーが見つかった場合には、送られてきたパスワードによる認証をauthenticateメソッドを使って行います
+      session[:user_id]=user.id
+      redirect_to root_path, notice: 'ログインしました'
     else
-      flash.now[:danger] = 'メールアドレスもしくはパスワードが誤っています'
-      render 'new'
+      render :new
     end
+    # user = User.find_by(email: params[:session][:email].downcase)
+    # if user && user.authenticate(params[:session][:password])
+    #   log_in user
+    #   redirect_to root_path
+    # else
+    #   flash.now[:danger] = 'メールアドレスもしくはパスワードが誤っています'
+    #   render 'new'
+    # end
   end
 
-  def destroy
-    log_out
-    redirect_to root_path
+
+　def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
   end
 
 end
